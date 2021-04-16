da125a6282e3cee94c100eb7b350bef06556ae82
velazquezabraham <58990476+velazquezabraham@users.noreply.github.com>
2021-04-16 11:28:30 -0700

    flask_python_1.py joining templates, added templates

--- flask_python_code/flask_python_1.py
+++ flask_python_code/flask_python_1.py
@@ -6,32 +6,16 @@
 #       working with templates tutorial 2
 
 
-
-
 #import flask tools
-from flask import Flask, redirect, url_for
+#added render_template to render html files and render them as our web pages
+from flask import Flask, redirect, url_for, render_template
 
 #create an app
 app = Flask(__name__) #__name__ is a key word
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>Hello<h1>"
-
-#route will pass a variable to user()
-#user() wi;; return an f string only available ion python3
-@app.route("/<name>")
-def user(name): 
-    return f"Hello {name}!" 
-
-#admin() will send the user to another website aka 'user' using the redirect
-#function- the redirect function takes as input the name of the website to be
-#re-directed to.
-#The user function takes an argument called 'name', therefor we need to make 
-#sure we pass an argument named 'name' and its value will be  "Admin"
-@app.route("/admin/")#added slash at the end to work with slash or no slash
-def admin():
-    return redirect(url_for("user", name = "Admin"))
+    return "Hello! This is the main page <h1>Hello</h1>"
 
 #run the app
 if __name__ == "__main__":
