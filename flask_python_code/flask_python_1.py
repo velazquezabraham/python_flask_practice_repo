04c8270f4aa6ffaf6e66005787507244734d2172
velazquezabraham <58990476+velazquezabraham@users.noreply.github.com>
2021-04-16 22:43:31 -0700

    Working with HTTP GET/POST added login.html

--- flask_python_code/flask_python_1.py
+++ flask_python_code/flask_python_1.py
@@ -8,15 +8,30 @@
 
 #import flask tools
 #added render_template to render html files and render them as our web pages
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 #create an app
 #__name__ is a key word
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html",urltag=name, content=["Ave","vela"])
+#We use get request by default 
+@app.route("/")
+def home():
+        return render_template("index.html")
+
+#we want to know the functions we can use with get and post
+#How did we reached this page; POST or GET? use the request method to find out
+@app.route("/login", methods=["POST", "GET"])
+def logic():
+    if request.method == "POST": #make sure you use capitals
+        user == request.form["nm"]#use the same variable name from the html login to use the same value. This only works with request POST
+        return redirect(url_for("user", usr=user))#passig the user value to the user func
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>in user function {usr}</h1>"
 
 #run the app
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 #kill the flask server if "Adress already in service" by using the following bas commands:
 	ps -fA | grep python3
 	kill -9 <number you want to kill>
+
+#tutorial 3 notes:
+#	working with 2 basic HTTP ways to send and receive data using GET/POST
+#	Sending info to our service or our clients
+#	Get - most common, but insecure. Usually pased through the url bar or use to redirect a link 
+#	POST - More secure way,
